11aa2a1a7f8409e5f0b251096cb1e953568885e5
MANUEL1518 <loquendo_118_loco@hotmail.com>
2021-06-05 16:11:22 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔💔💔💔 [¡¡INSERT A GRAND PROJECT HERE!!] 💔💔💔💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <Text style={styles.bigBlue}>💔</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'white',
+  }
+});
